27558a3b13b5143bb0a8dcd8c7de6ea6acde1bd9
sgujuluva <sanghee04@gmail.com>
2022-05-31 20:53:32 +0200

    using random gene

--- index.js
+++ index.js
@@ -6,45 +6,50 @@
 
  6. max length 6
  */
- let pwd = document.querySelector("#showMe");
+let pwd = document.querySelector("#showMe");
 
- function passwordNum(){
-     /* let result = [];
-    let numAndString = "0123456789abcdefghijklmnopqrstuvwxyz$&@%";
-    for(let i = 0; i < 8; i++){
-        result.push(numAndString[Math.floor(Math.random() * numAndString.length)]);
-    }
-    pwd.innerHTML = result.join(""); 
-    return pwd.innerHTML; */
-
-     let result = "";
-     let result1= [];
-     let number = "0123456789";
-     let lowerCase="abcdefghijklmnopqrstuvwxyz";
-     let uppercase = lowerCase.toUpperCase();
+function passwordNum() {
+    /* let result = [];
+      let numAndString = "0123456789abcdefghijklmnopqrstuvwxyz$&@%";
+      for(let i = 0; i < 8; i++){
+          result.push(numAndString[Math.floor(Math.random() * numAndString.length)]);
+      }
+      pwd.innerHTML = result.join("");
+      return pwd.innerHTML; */
+  let result1 = [];
+    let number = "0123456789";
+    let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    let uppercase = lowerCase.toUpperCase();
     let specialCharacters = "$@&%";
     let numberResult = [];
     let lowerCaseResult = [];
     let upperCaseResult = [];
     let specialCharactersResult = [];
-
-    for ( let i = 0; i < 3; i++){
-       // if(result1 !== []){
-            numberResult.push(number[Math.floor(Math.random() * number.length)]);
-            lowerCaseResult.push( lowerCaseResult[Math.floor(Math.random() * lowerCaseResult.length)]);
-            upperCaseResult.push( upperCaseResult[Math.floor(Math.random() * upperCaseResult.length)]);
-            specialCharactersResult.push(  specialCharactersResult[Math.floor(Math.random() *  specialCharactersResult.length)]);
-      //  }
-      
-
-    } 
-  
-    result += numberResult.join("")+lowerCaseResult.join("")+upperCaseResult.join("")+specialCharactersResult.join("");
-    console.log(result)
-    for(let i = 0; i < result.length; i++){
-        result1.push(result[Math.floor(Math.random()* result.length)]);
-        pwd.innerHTML = result1.join("");       
-    }
+    
+    for (let i = 0; i < 3; i++) {
+   
+      numberResult.push(number[Math.floor(Math.random() * number.length)]);
+      lowerCaseResult.push(
+        lowerCase[Math.floor(Math.random() * lowerCase.length)]
+      );
+      upperCaseResult.push(
+        uppercase[Math.floor(Math.random() * uppercase.length)]
+      );
+      specialCharactersResult.push(
+        specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+      );
      
- }
-passwordNum();
+    }
+    let result =
+      numberResult.join("") +
+      lowerCaseResult.join("") +
+      upperCaseResult.join("") +
+      specialCharactersResult.join("");
+      for (let i = 0; i < result.length; i++) {
+        result1.push(result[Math.floor(Math.random() * result.length)]);    
+    
+     pwd.innerHTML = result1.join("");
+      }
+    return result1.join("");
+  }
+  console.log(passwordNum());
